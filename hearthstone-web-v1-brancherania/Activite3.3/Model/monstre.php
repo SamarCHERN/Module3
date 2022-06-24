4f6f11f67f982f9e766d7b0fc6d67ea7402faa02
Samar Cherni <scherni@dom.tti>
2022-06-24 18:03:32 +0100

    Hstone

---
+++ hearthstone-web-v1-brancherania/Activite3.3/Model/monstre.php
@@ -0,0 +1,42 @@
+<?php
+include('carte.php');
+include('DamageableInterface.php');
+
+class Monstre extends Carte implements DamageableInterface{
+
+    private int $ptsVie;
+
+    public function __construct(){
+        parent::__construct();
+        $this->ptsVie = getPtsVie();   
+    }
+
+    public function getPtsVie(){
+        $this->ptsVieStatement=self::$db->prepare('SELECT vie FROM cartessort WHERE id= rand(1,10)');
+        $ptsVieStatement->execute();
+        $vie = $ptsVieStatement->fetchAll();
+        $this->ptsVie = $vie;
+        return $this->ptsVie;
+    }
+
+    public function takeDamages(int $n){
+        if ($this->ptsVie - $n <= 0) {
+            $this->ptsVie = 0;
+        }else {
+            $this->ptsVie = $this->ptsVie - $n;
+        }
+    }
+
+    public function attaquer(DamageableInterface $monstre){
+       $monstre->takeDamages($this->ptsDegats);
+    }
+}
+
+
+// $monstre1 = new Monstre(2,8,2);
+// $monstre2 = new Monstre(2,8,5);
+// // print_r($monstre2);
+// $monstre1->attaquer($monstre2);
+
+
+// // print_r($monstre2);
